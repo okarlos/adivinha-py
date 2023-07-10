bb6c997a76ff6e878b02e5a20b711ebef9684182
Karlos Kliemann <karloskliemann@gmail.com>
2023-07-10 07:46:01 -0300

    feat:start implementing hangman game

--- forca.py
+++ forca.py
@@ -3,5 +3,26 @@ def jogar():
     print("***************Bem-vindo ao jogo de Forca!****************")
     print("**********************************************************")
 
+    palavra_secreta = "teste"
+    acertos = ["_","_","_","_","_"]
+
+    enforcou = False
+    acertou = False
+
+    print(acertos)
+
+    while(not enforcou and not acertou):
+
+        chute = input("Digite uma letra: ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                acertos[index] = letra
+            index = index + 1
+        print(acertos)
+
+
 if(__name__ == "__main__"):
     jogar()
