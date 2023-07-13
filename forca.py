5149b5724e2fc907c452b14a35be5f43a5ebb425
Karlos Kliemann <karloskliemann@gmail.com>
2023-07-13 07:36:41 -0300

    feat:file reading and optimizing code

--- forca.py
+++ forca.py
@@ -1,44 +1,72 @@
+import random
+
 def jogar():
+    mensagem_boas_vindas()
+
+    palavra_secreta = sorteia_palavra_secreta()
+    
+    acertos = ["_" for letra in palavra_secreta] #cria uma lista com o mesmo tamanho de palavra_secreta
+
+    enforcou = False
+    erros = 0
+
+    print("\n", acertos, "\n")
+
+    while not enforcou and "_" in acertos:
+
+        #tentativas = tamanho da palavra secreta menos os erros
+        print("Você tem {} tentativa(s).".format(len(palavra_secreta)-erros))
+        chute = input("Digite uma letra: ").upper().strip()
+
+        if len(chute) != 1 or not chute.isalpha():
+            print("Digite apenas uma letra válida.")
+            continue
+
+        if chute in palavra_secreta:
+            marca_chute_correto(chute, palavra_secreta, acertos)
+        else:
+            erros += 1
+
+        #permite N erros, onde N é igual ao número de letras da palavra secreta
+        enforcou = erros == len(palavra_secreta) 
+
+        #valida se tem algum _ em acertos, se não tiver é porque a palavra foi descoberta
+        print(acertos)
+
+    if "_" not in acertos:
+        imprime_mensagem_vencedor()
+    else:
+        imprime_mensagem_perdedor(palavra_secreta)
+
+    print("Fim do jogo")
+
+def mensagem_boas_vindas():
     print("**********************************************************")
     print("***************Bem-vindo ao jogo de Forca!****************")
     print("**********************************************************")
 
-    palavra_secreta = "karlos".upper()
-    acertos = ["_" for letra in palavra_secreta] #cria uma lista com o mesmo tamanho de palavra_secreta
+def sorteia_palavra_secreta():
+    with open("palavras.txt") as arquivo:
+        palavras = []
+        for linha in arquivo:
+            palavras.append(linha.strip())
 
-    enforcou = False
-    acertou = False
-    erros = 0
+    index = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[index].upper()
+    return palavra_secreta
 
-    print("")
-    print(acertos)
-    print("")
+def marca_chute_correto(chute, palavra_secreta, acertos):
+    index = 0
+    for letra in palavra_secreta:
+        if(chute == letra):
+            acertos[index] = letra
+        index += 1 #mesma saída de index = index + 1
 
-    while(not enforcou and not acertou):
+def imprime_mensagem_vencedor():
+    print("Você ganhou!")
 
-        print("Você tem {} tentativa(s).".format(len(palavra_secreta)-erros)) #tentativas = tamanho da palavra secreta menos os erros
-        chute = input("Digite uma letra: ")
-        chute = chute.strip().upper()
-
-        if (chute in palavra_secreta):
-            index = 0
-            for letra in palavra_secreta:
-                if(chute == letra):
-                    acertos[index] = letra
-                index += 1 #mesma saída de index = index + 1
-        else:
-            erros += 1
-
-        enforcou = erros == len(palavra_secreta) #permite N erros, onde N é igual ao número de letras da palavra secreta
-        acertou = "_" not in acertos #valida se tem algum _ em acertos, se não tiver é porque a palavra foi descoberta
-        print(acertos)
-
-    if(acertou):
-        print("Você ganhou!")
-    else:
-        print("Vocês perdeu!")
-
-    print("Fim do jogo")
+def imprime_mensagem_perdedor(palavra_secreta):
+    print("Vocês perdeu! A palavra era {}.".format(palavra_secreta))
 
 if(__name__ == "__main__"):
     jogar()
